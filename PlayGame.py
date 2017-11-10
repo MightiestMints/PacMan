fadf656cc83e402ca03dc44caedd815deaaa46cf
jurentie <jurentie@rams.colostate.edu>
2017-11-09 20:18:01 -0700

    Added a function to get list of Dots spawning ghosts every 5 turns closes #10

--- PlayGame.py
+++ PlayGame.py
@@ -1,17 +1,37 @@
 import PacMan as P
 import Ghost as G
+import Dot as d
+
+# Scans through state and returns a list of Dot objects.
+def getDots(state):
+    dots = []
+    for x, i in enumerate(state):
+        for y, j in enumerate(state[x]):
+            if j is '.':
+                dots.append(d.Dot(x, y, False))
+            if j is 'o':
+                dots.append(d.Dot(x, y, True))
+    return dots
 
 # Creates Pac Man and starts the game. State is passed in as a parameter
 # Right now the state is advancing a turn only after a move is selected. This will change when Pac Man gets his smarts
 # Currently spawns a Ghost after the first turn
 def startGame(state):
     p = P.PacMan(state)
+    ghosts = []
+    dots = getDots(state)
     turn = 1
     while not p.gameOver():
         print("Lives:", p.getLives(), "\tDots left:", p.dotsLeft, "\tLocation:", p.location, "\tTurn:", turn)
+        if turn is 5:
+            ghosts.append(G.Ghost(state))
+            turn = 0
         p.printState(state)
         print("\nActions available:", p.actions(state))
         state = p.takeAction(state, input("Action: "))
+        for ghost in ghosts:
+            print('here')
+            state = ghost.randomMove(state)
         print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
         turn += 1
     if p.lives == 0:
@@ -24,7 +44,7 @@ if __name__ == "__main__":
     state = [['=', '=', '=', '=', '=', '=', '=', '=', '=', '=', '='],
              ['|', 'G', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'],
              ['|', ' ', '=', '=', '=', ' ', '=', '=', '=', ' ', '|'],
-             ['|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|' ],
+             ['|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|'],
              ['|', ' ', '=', '=', '=', ' ', '=', '=', '=', ' ', '|'],
              ['|', '.', '.', '.', '.', '.', '.', '.', '.', 'P', '|'],
              ['=', '=', '=', '=', '=', '=', '=', '=', '=', '=', '=']]
